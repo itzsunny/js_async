1afc301a7ebecb644fd65ea776f1a0432db71db4
sunny <sunnychoudhury007@gmail.com>
2019-10-09 22:25:52 +0530

    update assignments

--- 02 - XMLHttpRequest/github-user/main.js
+++ 02 - XMLHttpRequest/github-user/main.js
@@ -1,2 +1,41 @@
 // Your code goes here
 
+function getUser (event,str=null) {
+    
+    function getAvatar(src) {
+    document.querySelector('.user_img').src = src.avatar_url;
+    }
+
+    let input = document.getElementById('input').value;
+    console.log(input)
+    let xhr = new XMLHttpRequest();
+    xhr.onload = () => 
+        {
+        console.log(JSON.parse(xhr.response));
+        let data = JSON.parse(xhr.response);
+        document.querySelector('.display').style.backgroundColor = "rgba(0,0,0,0.1)";
+        document.getElementById('name').innerText = data.name;
+        document.getElementById('bio').innerText = data.bio;
+        document.getElementById('followers').innerText = data.followers_url;
+        document.getElementById('following').innerText = data.following_url;
+        document.getElementById('repos').innerText = data.repos_url;
+        document.getElementById('gists').innerText = data.gists_url;
+
+        if(str){
+            if(data.message == 'Not Found'){
+                document.querySelector('.logo_img').src = 'notfound.png';
+            } else {
+                document.querySelector('.logo_img').src = data.avatar_url;
+            }
+        } else{
+            getAvatar(data);
+        }
+        };
+
+    xhr.open("GET",`https://api.github.com/users/${input}`)
+    xhr.send();
+
+}
+document.querySelector('#input').addEventListener('keyup',(e) => getUser(e,'keyup') )
+
+document.querySelector('.search').addEventListener('click',getUser);
--- 03 - Promise/02 - fetch/fetch.js
+++ 03 - Promise/02 - fetch/fetch.js
@@ -4,6 +4,19 @@
 // 3. Resolve the promise when data is fetched (onload)
 // 4. Reject the promise when error occured (onerror)
 
-function fetchData() {
-  // your code goes here
+function fetchData(url,request = 'GET') {
+ 
+  return new Promise((resolve,reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open (request,url);
+    xhr.onload = (() => resolve(xhr.response))
+    xhr.onerror = (() => reject('Error !'))
+    xhr.send();
+
+  })
 }
+  fetchData('https://api.github.com/users/itzsunny')
+  // .then(res => res.json)
+  // .then(res => console.log(res))
+  // .catch(rej => console.log(rej))
